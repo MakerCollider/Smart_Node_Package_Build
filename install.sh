7959d4a4a88d41267df21033450786cb87f402d9
尹雨 <yinyu@inus-MacBook-Air.local>
2015-07-21 10:47:10 +0800

    fix the install script for new folder

--- install.sh
+++ install.sh
@@ -1,19 +1,27 @@
 CURR=`pwd`
 LOG=$CURR/install.log
 
+echo "updating mraa..."
+opkg install $CURR/lib/libmraa0_0.7.3_i586.ipk
+echo "done"
+
+echo "updating upm..."
+opkg install $CURR/lib/upm_0.3.1_i586.ipk
+echo "done"
+
 echo "cd /usr"
 cd /usr
 
 echo "install libuv..."
-tar -xzvf $CURR/libuv.bin.tgz &>> $LOG
+tar -xzvf $CURR/lib/libuv.bin.tgz &>> $LOG
 echo "done"
 
 echo "install opencv..."
-tar -xzvf $CURR/OpenCV-3.0.0-rc1.tgz &>> $LOG
+tar -xzvf $CURR/lib/OpenCV-3.0.0-rc1.tgz &>> $LOG
 echo "done"
 
 echo "install libv4l..."
-tar -xzvf $CURR/libv4l.tgz &>> $LOG
+tar -xzvf $CURR/lib/libv4l.tgz &>> $LOG
 echo "done"
 
 echo "log saved to $LOG"
