56a6ce27c6b9d8f6a1718a0b01a1ea24a6e67209
myloveed <myloveed@github.com>
2015-08-12 08:00:49 +0000

    add script for pack the release package automaticlly, and the release package support one click install. 	new file:   pub/install_head.sh 	new file:   pub/make_rel_pack.sh 	new file:   pub/package_exclude

---
+++ pub/install_head.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+line=`wc -l $0|awk '{print $1}'`
+line=`expr $line - 10` 
+rm -r io-js/ Atlas/ node-red/ lib/ README.md install.sh install_for_dev.sh  
+tail -n $line $0 |tar xzv
+./install.sh
+ret=$?
+#
+#
+#
+exit $ret
---
+++ pub/make_rel_pack.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+TAR_NAME=smart_node.tgz
+echo $TAR_NAME
+echo "make release package..."
+rm -rf $TAR_NAME
+tar -czvf $TAR_NAME -X ./package_exclude ../node-red/ ../Atlas/ ../io-js/ ../lib/ ../install.sh ../install_for_dev.sh ../README.md
+
+cat install_head.sh smart_node.tgz >smart_node.install
+
+chmod 755 ./smart_node.install
+
+rm ./smart_node.tgz
