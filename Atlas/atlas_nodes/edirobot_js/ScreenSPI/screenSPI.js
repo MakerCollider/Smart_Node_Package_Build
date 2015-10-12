a158b18d6f20f85c4eb9ecc3bc5bd10d9cb6c6a4
hifanso <hifanso@163.com>
2015-10-12 17:39:54 +0000

    add SPI Screen node

---
+++ Atlas/atlas_nodes/edirobot_js/ScreenSPI/screenSPI.js
@@ -0,0 +1,43 @@
+module.exports = function(RED) {
+
+    var jslib = require('jsupm_screenSpi9225');
+    var exec = require('child_process').exec; 
+
+    function ScreenSPI(config) {
+        console.log("SPI 9225 Screen initalizing.......");
+        RED.nodes.createNode(this, config);
+
+        var cmd = "echo on > /sys/devices/pci0000\:00/0000\:00\:07.1/power/control";
+        exec(cmd, function(err,stdout,stderr) {
+            if(err) {
+                console.log("enable power control error:"+stderr);
+            } else {
+                console.log("enable power control ok");
+            }
+        }); 
+    
+        var screen = new jslib.ScreenSpi9225(7,5,6);
+        screen.ILI9225GclearScreen(0x0000); 
+        this.status({fill:"blue",shape:"dot",text:"Initalized"});
+        
+        //Handle inputs
+        this.on('input', function(msg) {
+            if(msg.payload == 1) {
+                screen.ILI9225GclearScreen(0x0000);
+                screen.ILI9225GfillRect(10,10,24,36, 0xffff);
+                this.status({fill:"blue",shape:"dot",text:"showing"});
+            } else {
+                screen.ILI9225GclearScreen(0x0000);
+                this.status({fill:"red",shape:"ring",text:"clear"});
+            }
+        });
+
+        this.on('close', function() {
+            console.log("Stop Screen");
+            screen.ILI9225GclearScreen(0x0000); 
+            delete screen; 
+            this.status({fill:"red",shape:"ring",text:"closed"});
+        });
+    }
+    RED.nodes.registerType("ScreenSPI", ScreenSPI);
+}
