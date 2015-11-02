1fecb8b481f8ae80b180b775ad02ef9642cc2914
Yinyu <hifanso@163.com>
2015-11-02 06:09:18 +0000

    fix picture crash issue

--- Atlas/atlas_nodes/edirobot/ScreenSPI/screenSPI.js
+++ Atlas/atlas_nodes/edirobot/ScreenSPI/screenSPI.js
@@ -6,7 +6,6 @@ module.exports = function(RED) {
     function ScreenSPI(config) {
         console.log("SPI 9225 Screen initalizing.......");
         RED.nodes.createNode(this, config);
-	var node = this;
 
         var cmd = "echo on > /sys/devices/pci0000\:00/0000\:00\:07.1/power/control";
         exec(cmd, function(err,stdout,stderr) {
@@ -25,11 +24,16 @@ module.exports = function(RED) {
         this.on('input', function(msg) {
             if(msg.payload == 1) {
                 screen.ILI9225GclearScreen(0x0000);
-                screen.ILI9225GfillRect(0);
-                this.status({fill:"blue",shape:"dot",text:"showing"});
+                screen.ILI9225GfillRect(1);
+                this.status({fill:"blue",shape:"dot",text:"happy!"});
+            } else if (msg.payload == 0) {
+                screen.ILI9225GclearScreen(0x0000);
+                screen.ILI9225GfillRect(0);                  
+                this.status({fill:"blue",shape:"dot",text:"angry"});
             } else {
                 screen.ILI9225GclearScreen(0x0000);
-                this.status({fill:"red",shape:"ring",text:"clear"});
+                screen.ILI9225GfillRect(2);  
+                this.status({fill:"blue",shape:"dot",text:"normal"});
             }
         });
 
