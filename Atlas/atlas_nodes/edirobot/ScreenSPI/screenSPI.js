82aa71eddf22774907f746288f648e058f790da9
TianYe <flymaxty@gmail.com>
2015-11-06 18:08:50 +0800

    Fix ScreenSPI and Driver nodes.

--- Atlas/atlas_nodes/edirobot/ScreenSPI/screenSPI.js
+++ Atlas/atlas_nodes/edirobot/ScreenSPI/screenSPI.js
@@ -6,6 +6,7 @@ module.exports = function(RED) {
     function ScreenSPI(config) {
         console.log("SPI 9225 Screen initalizing.......");
         RED.nodes.createNode(this, config);
+	var node = this;
 
         var cmd = "echo on > /sys/devices/pci0000\:00/0000\:00\:07.1/power/control";
         exec(cmd, function(err,stdout,stderr) {
@@ -16,7 +17,7 @@ module.exports = function(RED) {
             }
         }); 
     
-        var screen = new jslib.ScreenSpi9225(7,5,6);
+        var screen = new jslib.ScreenSpi9225(12,18,19);
         screen.ILI9225GclearScreen(0x0000); 
         this.status({fill:"blue",shape:"dot",text:"Initalized"});
         
@@ -24,16 +25,11 @@ module.exports = function(RED) {
         this.on('input', function(msg) {
             if(msg.payload == 1) {
                 screen.ILI9225GclearScreen(0x0000);
-                screen.ILI9225GfillRect(1);
-                this.status({fill:"blue",shape:"dot",text:"happy!"});
-            } else if (msg.payload == 0) {
-                screen.ILI9225GclearScreen(0x0000);
-                screen.ILI9225GfillRect(0);                  
-                this.status({fill:"blue",shape:"dot",text:"angry"});
+                screen.ILI9225GfillRect(0);
+                this.status({fill:"blue",shape:"dot",text:"showing"});
             } else {
                 screen.ILI9225GclearScreen(0x0000);
-                screen.ILI9225GfillRect(2);  
-                this.status({fill:"blue",shape:"dot",text:"normal"});
+                this.status({fill:"red",shape:"ring",text:"clear"});
             }
         });
 
@@ -46,3 +42,4 @@ module.exports = function(RED) {
     }
     RED.nodes.registerType("ScreenSPI", ScreenSPI);
 }
+
